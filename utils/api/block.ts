6c4feea5e3f4e07bdd5034646abc622a9093ad0f
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 09:36:39 +0530

    ft: add routes for block

---
+++ utils/api/block.ts
@@ -0,0 +1,49 @@
+import { api } from "@/config/api";
+import { BlockSchema } from "@/types/apiTypes";
+
+export const createBlock = async (block: Block): Promise<BlockSchema[]> => {
+  try {
+    return await api("/flow", {
+      method: "POST",
+      body: JSON.stringify(block),
+    });
+  } catch (error) {
+    console.error("Error creating block", error);
+  }
+  return [{} as BlockSchema];
+};
+
+export const getBlockById = async (id: string): Promise<BlockSchema[]> => {
+  try {
+    return await api(`/block/:${id}`, {
+      method: "GET",
+    });
+  } catch (error) {
+    console.log("Error while getting block", error);
+  }
+  return [{} as BlockSchema];
+};
+
+export const getAllBlocks = async (): Promise<BlockSchema[]> => {
+  try {
+    return await api("/block/", {
+      method: "GET",
+    });
+  } catch (error) {
+    console.log("Error while getting blocks", error);
+  }
+  return [{} as BlockSchema];
+};
+
+export const getBlocksByCreator = async (
+  creator_address: string,
+): Promise<BlockSchema[]> => {
+  try {
+    return await api(`/block/: ${creator_address}`, {
+      method: "GET",
+    });
+  } catch (error) {
+    console.log("Error while getting blocks", error);
+  }
+  return [{} as BlockSchema];
+};
