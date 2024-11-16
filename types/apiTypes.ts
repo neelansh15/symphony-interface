e35777ac2af8e461a6d173f8c397f54733bb042d
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 09:31:33 +0530

    ft: add api routes for flow/workflow

---
+++ types/apiTypes.ts
@@ -0,0 +1,34 @@
+export type BlockSchema = {
+  id: number;
+  created_at: string;
+  created_by: string;
+  name: string;
+  description: string;
+  location: string;
+  vcs_path: string;
+  params: any;
+};
+
+export type FlowSchema = {
+  id: number;
+  name: string;
+  description: string;
+  trigger_type: string;
+  trigger_condition: string;
+  block_sequence: number[];
+  block_params: any[];
+  created_by: string;
+};
+
+export type JobStatusSchema = {
+  id: number;
+  flow_id: number;
+  created_at: string;
+  status: string;
+  details: string;
+};
+
+export type Result<T = string> = {
+  status: number;
+  data: T;
+};
