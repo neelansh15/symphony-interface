6047550f7f82697d7cce00e4053cee5f1bca4635
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 09:47:12 +0530

    ft: add routes for job status

---
+++ utils/api/jobStatus.ts
@@ -0,0 +1,54 @@
+import { api } from "@/config/api";
+import { JobStatusSchema } from "@/types/apiTypes";
+
+export const createJob = async (
+  job: JobStatusSchema,
+): Promise<JobStatusSchema[]> => {
+  try {
+    return await api("/job_status/", {
+      method: "POST",
+      body: JSON.stringify(job),
+    });
+  } catch (error) {
+    console.error("Error creating job", error);
+  }
+  return [{} as JobStatusSchema];
+};
+
+export const getJobById = async (id: string): Promise<JobStatusSchema[]> => {
+  try {
+    return await api(`/job_status/:${id}`, {
+      method: "GET",
+    });
+  } catch (error) {
+    console.log("Error while getting job", error);
+  }
+  return [{} as JobStatusSchema];
+};
+
+export const getJobStatusByStatus = async (
+  status: string,
+): Promise<JobStatusSchema[]> => {
+  try {
+    return await api(`/job_status/status/:${status}`, {
+      method: "GET",
+    });
+  } catch (error) {
+    console.log("Error while getting jobs", error);
+  }
+  return [{} as JobStatusSchema];
+};
+
+export const updateJobStatus = async (
+  id: string,
+  job_status: JobStatusSchema,
+): Promise<void> => {
+  try {
+    await api(`/job_status/:${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(job_status),
+    });
+  } catch (error) {
+    console.error("Error while updating job status", error);
+  }
+};
